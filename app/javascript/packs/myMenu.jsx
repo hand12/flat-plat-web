545c5d474a55ef4099228cb00c133d1e1f8051f6
Yusuke Yamashita <yusuke.yamashita@livesense.co.jp>
2017-10-10 11:02:51 +0900

    送信一歩手前

--- app/javascript/packs/myMenu.jsx
+++ app/javascript/packs/myMenu.jsx
@@ -7,12 +7,36 @@ import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import { Calendar } from './calendar.jsx'
 
-const Content = props => (
-    <div id="content">
-      {/* <TopMenu /> */}
-      <MainContainer/>
-    </div>
-)
+class Content extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      location: '',
+      start_date: '',
+      finish_date: '',
+      description: '',
+    }
+  }
+
+  onChangeField = (e) => {
+    this.setState({[e.target.name]: e.target.value});
+    console.log(this.state)
+  }
+
+  onSubmit = () => {
+    console.log("API叩く")
+    console.log(this.state)
+  }
+
+  render(){
+    return (
+      <div id="content">
+        {/* <TopMenu /> */}
+        <MainContainer { ...this.state } onChangeField = { this.onChangeField } onSubmit = {this.onSubmit} />
+      </div>
+    )
+  }
+}
 
 const TopMenu = props => (
   <div id="top_menu">
@@ -22,29 +46,22 @@ const TopMenu = props => (
   </div>
 )
 
-const MainContainer = props => {
-  const handleSubmit = () => {
-    console.log("ほげ")
-  }
+const MainContainer = props => (
+  <div id="main_container">
+    <form action="javascript:void(0)" onSubmit = { props.onSubmit } >
+      <LocationField onChangeField = { props.onChangeField } />
+      <DateField onChangeField = { props.onChangeField } />
+      <DescriptionField onChangeField = { props.onChangeField } />
+      <button type="submit" className="submit" >プランを投稿する</button>
+    </form>
+  </div>
+)
 
-  const setValue = (value) => {
-    console.log(value)
-  }
-  return (
-    <div id="main_container">
-      <form action="javascript:void(0)" onSubmit={ handleSubmit }>
-        <PlaceField setValue = { setValue } />
-        <DateField setValue = { setValue } />
-        <DescriptionField setValue = { setValue } />
-        <button type="submit" className="submit" >プランを投稿する</button>
-      </form>
-    </div>
-  )
-}
 
-const PlaceField = props => (
+
+const LocationField = props => (
   <div id="place_field">
-    <input placeholder="どちらまで" onChange = { e => props.setValue(e.target.value) } />
+    <input placeholder="どちらまで" name="location" onChange = { props.onChangeField } />
   </div>
 )
 
@@ -52,18 +69,18 @@ const DateField = props => (
   <div id="date_field">
     <div className="date_container">
       <label htmlFor="departure_date">いつから</label>
-      <input type="date" id="departure_date" onChange = { e => props.setValue(e.target.value) } />
+      <input type="date" id="departure_date" name="start_date" onChange = { props.onChangeField } />
     </div>
     <div className="date_container">
       <label htmlFor="come_back_date">いつまで</label>
-      <input type="date" id="come_back_date" onChange = { e => props.setValue(e.target.value) } />
+      <input type="date" id="come_back_date" name="finish_date" onChange = { props.onChangeField } />
     </div>
   </div>
 )
 
 const DescriptionField = props => (
   <div id="description_field">
-    <input placeholder="どんな旅にしたいですか？" onChange = { e => props.setValue(e.target.value) } />
+    <input placeholder="どんな旅にしたいですか？" name="description" onChange = { props.onChangeField } />
   </div>
 )
 
